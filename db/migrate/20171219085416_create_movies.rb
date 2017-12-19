b1fe45fb8f8311e59b9afca324b8e1488e1f1e08
Waclaw Gajda <waclaw.gajda@gmail.com>
2017-12-19 10:11:11 +0100

    model, plik migracji

---
+++ db/migrate/20171219085416_create_movies.rb
@@ -0,0 +1,23 @@
+class CreateMovies < ActiveRecord::Migration[5.1]
+  def change
+    create_table :movies do |t|
+      t.string :title
+      t.date :relese_date
+      t.integer :length
+      t.text :contents
+      t.string :type
+      t.string :director
+      t.string :scenarist
+      t.string :country
+
+      t.timestamps
+    end
+    create_table :rewiewer do |t|
+      t.string :name
+      t.float :rating
+      t.text :comment
+
+      t.timestamps
+    end
+  end
+end
