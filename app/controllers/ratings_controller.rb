87f688249cb8c3d52662da8fac12408e2b92af99
Waclaw Gajda <waclaw.gajda@gmail.com>
2017-12-22 14:23:33 +0100

    metody rating

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -1,2 +1,11 @@
 class RatingsController < ApplicationController
+  def create
+    @movie = Movie.find(params[:movie_id])
+    @rating = Rating.new(movie_id: @movie_id) #nie rozumiem argumentów w nawisie
+
+  end
+  def destroy
+    @rating = Rating.find(params[:id])
+    @rating.destroy
+  end
 end
