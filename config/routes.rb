458010bbcd587269e354a381fc5d45cc005f6da6
Waclaw Gajda <waclaw.gajda@gmail.com>
2017-12-28 13:50:36 +0100

    rating nie działa

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :movies
+  resources :movies do
+    resources :ratings, only: [:create, :destroy]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
